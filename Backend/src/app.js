a6c28c57be5a76bc054392cd2872d405b6afe690
tauseefaskari110 <tauseefaskari110@gmail.com>
2026-09-20 10:51:50 +0530

    Add quick post app frontend and backend

---
+++ Backend/src/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const postModel = require('./models/post.model')
+const multer = require('multer')
+const uploadFile = require('./services/storage.service')
+const cors = require('cors')
+
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const upload = multer({ storage : multer.memoryStorage() })
+
+//POST /posts => create a post
+app.post("/create-post", upload.single('image'), async (req, res) => {
+
+    const result = await uploadFile(req.file.buffer);
+
+    const post = await postModel.create({
+        image: result.url,
+        caption: req.body.caption
+    })
+
+   return res.status(200).json({
+        message:"Posts Created Successfully",
+        data: post
+    })
+})
+
+app.get("/posts", async (req, res) => {
+   const post =  await postModel.find()
+
+   return res.status(200).json({
+    message : "Posts Fetched Successfully",
+    post
+   })
+})
+
+
+module.exports = app;
---
+++ Frontend/src/pages/Feed.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const Feed = () => {
+
+    const [posts, setPosts] = useState([
+      {  _id:"1",
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR81bNIEaGCzdwl2QV5sIrD4JXpsyk1smkr4psQ6MhDvA8lGy5_6Y4sEyk&s=10",
+        caption:"A beautiful sunset over the mountains."
+      },           
+    ])
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/posts')
+        .then((response) => {
+            setPosts(response.data.post)
+        })
+        .catch((error) => {
+            console.error('Error fetching posts:', error);
+        });
+    }, [])
+
+    
+  return (
+
+    <section className='feed-section'>
+
+        {
+            posts.length > 0 ? (
+                posts.map((post) => (
+                    <div key={post._id} className='post-card'>
+                        <img src={post.image} alt={post.caption} />
+                        <p>{post.caption}</p>
+                    </div>
+                ))
+            ) : (
+                <h1>No posts available.</h1>
+            )
+        }
+    </section>
+    
+  )
+}
+
+export default Feed
